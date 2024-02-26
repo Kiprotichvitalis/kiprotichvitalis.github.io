deaf492c80d20c3803adcbd1ae9524d4c255275a
Kiprotichvitalis <vkutto@kabarak.ac.ke>
2024-02-26 19:48:42 +0300

    new

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const Product = require('./models/productmodel'); // Corrected import statement
+const Product = require('./models/productmodel');
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded({extended: false}))
+app.use(express.urlencoded({ extended: false }));
 
-app.get('/', (req, res) => {
-    res.send('Hello NODE API');
+app.get('/', async (req, res) => {
+    try {
+        const products = await Product.find({});
+        res.status(200).json(products);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
 });
 
 app.get('/products', async (req, res) => {
@@ -22,7 +27,7 @@ app.get('/products', async (req, res) => {
 app.get('/products/:id', async (req, res) => {
     try {
         const { id } = req.params;
-        const product = await Product.findById(id); // Corrected method name to findById
+        const product = await Product.findById(id);
         res.status(200).json(product);
     } catch (error) {
         res.status(500).json({ message: error.message });
@@ -44,34 +49,40 @@ app.post('/product', async (req, res) => {
         res.status(500).json({ error: 'Internal server error' });
     }
 });
+
 // update a product
-app.put('/products/:id', async(req, res) => {
-    try{
-    const {id} = req.params;
-    const product = await Product.findByIdAndUpdate (id, req.body);
-    // we cannot find any product in database
-    if(!product){
-    return res.status(404).json({message: 'cannot find any product with ID ${id}'})
-    }
-    const updatedproduct = await Product.findById(id);
-    res.status(200).json(updatedproduct);
-}catch (error) {
-    res.status(500).json({message: error.message})
-}
-})
-// delete a product
-app.delete('/products/:id'), async(req,res) =>{
-    try{
-        const {id} = req.params;
-        const product = await Product.findByIdAndDelete(id);
-        if(!product){
-            return res.status(404).json({message: 'cannot find any product with ID ${id}'})
+app.put('/products/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        const product = await Product.findByIdAndUpdate(id, req.body);
+
+        if (!product) {
+            return res.status(404).json({ message: `Cannot find any product with ID ${id}` });
         }
-res.status(200).json(product);
-    }catch(error){
-        res.status(500).json({message: error.message})
+
+        const updatedproduct = await Product.findById(id);
+        res.status(200).json(updatedproduct);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
     }
-}
+});
+
+// delete a product
+app.delete('/products/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        const product = await Product.findByIdAndDelete(id);
+
+        if (!product) {
+            return res.status(404).json({ message: `Cannot find any product with ID ${id}` });
+        }
+
+        res.status(200).json(product);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+});
+
 mongoose
     .connect('mongodb+srv://admin:Admin123@kuttoapi.9rzgtrd.mongodb.net/Node-API?retryWrites=true&w=majority&appName=kuttoAPI')
     .then(() => {
